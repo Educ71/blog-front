5e5a2092975bfbbe469d336ac62c6620946085db
Educ71 <luis.educ71@gmail.com>
2021-11-07 23:10:50 -0300

    adding login and registration pages

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,23 +1,60 @@
-import { FormField } from 'components/form-field';
+import { ApolloError } from '@apollo/client';
+import { Button } from 'components/button';
+import { TextField } from 'components/text-field';
+import { useLoginMutation } from 'data/graphql/mutation/login';
+import { LoginMutation } from 'data/graphql/mutation/login/login-user.mutation.gql';
+import Router from 'next/router';
 import React from 'react';
 
 export default function Login() {
+  const handleLoginComplete = (data: LoginMutation) => {
+    sessionStorage.setItem('@auth-key', data?.login?.accessToken);
+
+    Router.push('/index');
+  };
+
+  const handleLoginError = (error: ApolloError) => {
+    alert(error.message);
+  };
+
+  const [login, { loading }] = useLoginMutation(
+    handleLoginComplete,
+    handleLoginError,
+  );
+
+  const handleLoginSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = event.target as typeof event.target & {
+      userId: { value: string };
+      password: { value: string };
+    };
+
+    const email = formData.userId.value;
+    const password = formData.password.value;
+
+    login({ variables: { data: { email, password } } });
+  };
+
   return (
     <div className="bg-white p-10 rounded border border-pink-900">
       <h1 className="font-bold text-2xl mb-3">Login</h1>
-      <form
-        className="flex flex-col"
-        onSubmit={(event) => event.preventDefault()}
-      >
-        <FormField label="User ID" placeholder="Ex. admin" type="text" />
-
-        <FormField label="Password" placeholder="Ex. 12345" type="password" />
-
-        <input
-          type="submit"
-          value="Login"
-          className="rounded bg-pink-900 text-white font-bold text-lg p-1 self-start px-10 shadow-lg cursor-pointer"
+      <form className="flex flex-col" onSubmit={handleLoginSubmit}>
+        <TextField
+          label="User ID"
+          placeholder="Ex. admin"
+          type="text"
+          name="userId"
         />
+
+        <TextField
+          label="Password"
+          placeholder="Ex. 12345"
+          type="password"
+          name="password"
+        />
+
+        <Button loading={loading}>Login</Button>
       </form>
     </div>
   );
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,24 +1,107 @@
-import { FormField } from 'components/form-field';
+import { ApolloError } from '@apollo/client';
+import { Button } from 'components/button';
+import { TextField } from 'components/text-field';
+import { TextareaField } from 'components/textarea-field';
+import { useSignUpMutation } from 'data/graphql/mutation/sign-up';
+import Router from 'next/router';
 import React from 'react';
+import { validateEmail, validatePassword } from 'utils/field-validators';
 
 export default function Login() {
+  const handleCreateUserComplete = () => {
+    Router.push('/login');
+  };
+
+  const handleCreateUserError = (error: ApolloError) => {
+    alert(error.message);
+  };
+
+  const [createUser, { loading }] = useSignUpMutation(
+    handleCreateUserComplete,
+    handleCreateUserError,
+  );
+
+  const handleCreateUserSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = event.target as typeof event.target & {
+      email: { value: string };
+      name: { value: string };
+      bio?: { value: string };
+      password: { value: string };
+    };
+
+    const data = {
+      email: formData.email.value,
+      name: formData.name.value,
+      bio: formData.bio?.value,
+      password: formData.password.value,
+    };
+
+    const isDataValid = validateData(data);
+
+    isDataValid && createUser({ variables: { data } });
+  };
+
   return (
     <div className="bg-white p-10 rounded border border-pink-900">
-      <h1 className="font-bold text-2xl mb-3">Register</h1>
-      <form
-        className="flex flex-col"
-        onSubmit={(event) => event.preventDefault()}
-      >
-        <FormField label="User ID" placeholder="Ex. admin" type="text" />
-
-        <FormField label="Password" placeholder="Ex. 12345" type="password" />
-
-        <input
-          type="submit"
-          value="Register"
-          className="rounded bg-pink-900 text-white font-bold text-lg p-1 self-start px-10 shadow-lg cursor-pointer"
+      <h1 className="font-bold text-2xl mb-3">Registration form</h1>
+      <form className="flex flex-col" onSubmit={handleCreateUserSubmit}>
+        <TextField
+          label="Name"
+          placeholder="Ex. John Travolta"
+          type="text"
+          name="name"
         />
+
+        <TextField
+          label="Email"
+          placeholder="Ex. user@email.com"
+          type="email"
+          name="email"
+        />
+
+        <TextareaField
+          label="Bio"
+          placeholder="Ex. Actor and dancer"
+          name="bio"
+        />
+
+        <TextField
+          label="Password"
+          placeholder="Ex. 12345"
+          type="password"
+          name="password"
+        />
+
+        <Button loading={loading}>Register</Button>
       </form>
     </div>
   );
 }
+
+const validateData = (data: {
+  email: any;
+  name: any;
+  bio?: string | undefined;
+  password: any;
+}) => {
+  if (!validateEmail(data.email)) {
+    alert('Email should be valid. Example: user@register.com');
+    return false;
+  }
+
+  if (!validatePassword(data.password)) {
+    alert(
+      'Password should be minimum eight in length have at least one upper case letter, one lower case letter, one digit and one special character',
+    );
+    return false;
+  }
+
+  if (!data?.name) {
+    alert('Name is required');
+    return false;
+  }
+
+  return true;
+};
